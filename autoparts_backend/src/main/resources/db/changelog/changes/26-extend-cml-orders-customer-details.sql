3f7466bcddb5d848ec7c14e076a48bc5ce2acafc
Dynamite01 <behruzbektohtamishov@gmail.com>
2025-10-30 17:42:01 +0500

    order creating

--- autoparts_backend/src/main/resources/db/changelog/changes/26-extend-cml-orders-customer-details.sql
+++ autoparts_backend/src/main/resources/db/changelog/changes/26-extend-cml-orders-customer-details.sql
@@ -1,10 +1,10 @@
 --liquibase formatted sql
 --changeset autoparts:26-extend-cml-orders-customer-details
 ALTER TABLE cml_orders
-    ADD COLUMN customer_first_name VARCHAR(255),
-    ADD COLUMN customer_last_name VARCHAR(255),
-    ADD COLUMN customer_middle_name VARCHAR(255),
-    ADD COLUMN customer_state VARCHAR(255),
-    ADD COLUMN customer_type VARCHAR(255),
-    ADD COLUMN customer_office_address VARCHAR(500),
-    ADD COLUMN customer_user_id BIGINT;
+    ADD COLUMN IF NOT EXISTS customer_first_name VARCHAR(255),
+    ADD COLUMN IF NOT EXISTS customer_last_name VARCHAR(255),
+    ADD COLUMN IF NOT EXISTS customer_middle_name VARCHAR(255),
+    ADD COLUMN IF NOT EXISTS customer_state VARCHAR(255),
+    ADD COLUMN IF NOT EXISTS customer_type VARCHAR(255),
+    ADD COLUMN IF NOT EXISTS customer_office_address VARCHAR(500),
+    ADD COLUMN IF NOT EXISTS customer_user_id BIGINT;
